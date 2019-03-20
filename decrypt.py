11709214c1869e6c7c15f366341d46e694961f92
Mai Chi Cuong <31822056+wishuhere@users.noreply.github.com>
2019-03-20 20:22:03 +0700

    Add files via upload

--- decrypt.py
+++ decrypt.py
@@ -1,95 +1,95 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#Module co ban
-import sys, getopt, struct
-#Module PyCrypto
+import sys
+import getopt
+import struct
+# Module PyCrypto
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5, SHA256
 import Crypto.Util.Counter
 
-blockSize = 2048 #Chon blocksize la boi cua 16
+blockSize = 2048  # The blockSize is a mutilple of 16
 
-#=======================Decrypt=================================
+# ======================= Decrypt =================================
+
+
 def decrypt(mode, myKey, input_file, output_file):
 
-	if (mode != "ECB" and mode != "CBC" and mode != "CFB" and mode != "OFB" and mode != "CTR"):
-		print "Available modes: ECB, CBC, CFB, OFB, CTR."
-		print "Choose again!"
-		sys.exit()
+    if (mode != "ECB" and mode != "CBC" and mode != "CFB" and mode != "OFB" and mode != "CTR"):
+        print "Available modes: ECB, CBC, CFB, OFB, CTR."
+        print "Choose again!"
+        sys.exit()
 
-	if (mode == "ECB"):
-		mode = AES.MODE_ECB
-	elif (mode == "CFB"):
-		mode = AES.MODE_CFB
-	elif (mode == "OFB"):
-		mode = AES.MODE_OFB
-	elif (mode == "CTR"):
-		mode = AES.MODE_CTR
-	elif (mode == "CBC"):
-		mode =  AES.MODE_CBC
+    if (mode == "ECB"):
+        mode = AES.MODE_ECB
+    elif (mode == "CFB"):
+        mode = AES.MODE_CFB
+    elif (mode == "OFB"):
+        mode = AES.MODE_OFB
+    elif (mode == "CTR"):
+        mode = AES.MODE_CTR
+    elif (mode == "CBC"):
+        mode = AES.MODE_CBC
 
-	#Tao key 32bit bang ham bam tu key mac dinh
-	myHash = SHA256.new(myKey)
-	key = myHash.digest()
+    myHash = SHA256.new(myKey)
+    key = myHash.digest()
 
-	with open(input_file, "rb") as fin:
-		size = struct.unpack('<Q',fin.read(struct.calcsize('<Q')))[0]
-		size = long(size)
-		IV = fin.read(16) 
+    with open(input_file, "rb") as fin:
+        size = struct.unpack('<Q', fin.read(struct.calcsize('<Q')))[0]
+        size = long(size)
+        IV = fin.read(16)
 
-		if (mode != AES.MODE_CTR):
-			decryptor = AES.new(key, mode, IV)
-		else:
-			ctr = Crypto.Util.Counter.new(128, 			              initial_value=long(IV.encode("hex"), 16))
-			decryptor = Crypto.Cipher.AES.new(key, AES.MODE_CTR, counter=ctr)
+        if (mode != AES.MODE_CTR):
+            decryptor = AES.new(key, mode, IV)
+        else:
+            ctr = Crypto.Util.Counter.new(
+                128, 			              initial_value=long(IV.encode("hex"), 16))
+            decryptor = Crypto.Cipher.AES.new(key, AES.MODE_CTR, counter=ctr)
+
+        with open(output_file, "wb") as fout:
+            while True:
+                block = fin.read(blockSize)
+                if len(block) == 0:
+                    break
+                content = decryptor.decrypt(block)
+                fout.write(content)
+            fout.truncate(size)  # Remove padding
+    return
+
+# ===========================================================
 
 
-		with open(output_file, "wb") as fout:
-			while True:
-				block = fin.read(blockSize)
-				if len(block) == 0:
-					break
-				content = decryptor.decrypt(block)
-				#print content
-				fout.write(content)
-			fout.truncate(size)	#Xoa padding
-	return
-
-#===========================================================
 def main(argv):
-	#Xu ly tham so dong lenh
-	try:
-		opts, args = getopt.getopt(argv,"m:k:")
-	except getopt.GetoptError:
-		print "decrypt.py –m <mode> -k <key> <infile> <outfile>"
-		sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "m:k:")
+    except getopt.GetoptError:
+        print "decrypt.py –m <mode> -k <key> <infile> <outfile>"
+        sys.exit(2)
 
-	#Neu danh sach tham so sau khi tru di cac option
-	#khong phai la dang: "decrypt.py <input_file> <output_file>"
-	if(len(args) != 2):
-		print "decrypt.py –m <mode> -k <key> <input_file> <output_file>"
-		sys.exit(2)
+    if(len(args) != 2):
+        print "decrypt.py –m <mode> -k <key> <input_file> <output_file>"
+        sys.exit(2)
 
-	#Lay thong tin input_file va output_file
-	input_file = args[0]
-	output_file = args[1]
+    # get input_file and output_file
+    input_file = args[0]
+    output_file = args[1]
 
-	#Chay vong for de lay option
-	for opt, arg in opts:
-		if opt == "-h":
-		       print "decrypt.py c–m <mode> -k <key> <infile> <outfile>"
-	               sys.exit()
-		elif opt == "-m":
-			mode = arg.upper()
-		elif opt == "-k":
-			myKey = arg
-	#---------------Xu ly xong tham so dong lenh-------------------
+    # Getoption
+    for opt, arg in opts:
+        if opt == "-h":
+            print "decrypt.py c–m <mode> -k <key> <infile> <outfile>"
+            sys.exit()
+        elif opt == "-m":
+            mode = arg.upper()
+        elif opt == "-k":
+            myKey = arg
 
-	#Goi ham decrypt
-	decrypt(mode, myKey, input_file, output_file)
-	return
-#=================================================================
+    # Run decrypt function
+    decrypt(mode, myKey, input_file, output_file)
+    return
+# =================================================================
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- encrypt.py
+++ encrypt.py
@@ -1,108 +1,107 @@
 #!/usr/bin/python
 # coding=utf-8
 
-#Module co ban
-import sys, getopt, os, struct
-#Module PyCrypto
+import sys
+import getopt
+import os
+import struct
+# Module PyCrypto
 from Crypto.Cipher import AES
-#Module ham bam hash
+# Module hash function
 from Crypto.Hash import MD5, SHA256
-import  Crypto.Util.Counter
+import Crypto.Util.Counter
 
-blockSize = 2048 #Chon blocksize la boi cua 16
+blockSize = 2048  # The blockSize is a mutilple of 16
 padding = "@"
 
-#=======================Encrypt=================================
+# ======================= Encrypt =================================
+
+
 def encrypt(mode, myKey, IV, input_file, output_file):
 
-	if( mode != "ECB" and mode != "CBC" and mode != "CFB" and mode != "OFB" and mode != "CTR"):
-		print "Available modes: ECB, CBC, CFB, OFB, CTR."
-		print "Choose again!"
-		sys.exit()
+    if(mode != "ECB" and mode != "CBC" and mode != "CFB" and mode != "OFB" and mode != "CTR"):
+        print "Available modes: ECB, CBC, CFB, OFB, CTR."
+        print "Choose again!"
+        sys.exit()
 
-	if(mode == "ECB"):
-		mode = AES.MODE_ECB
-	elif (mode == "CFB"):
-		mode = AES.MODE_CFB
-	elif (mode == "OFB"):
-		mode = AES.MODE_OFB
-	elif (mode == "CBC"):
-		mode =  AES.MODE_CBC
-	elif (mode == "CTR"):
-		mode = AES.MODE_CTR
+    if(mode == "ECB"):
+        mode = AES.MODE_ECB
+    elif (mode == "CFB"):
+        mode = AES.MODE_CFB
+    elif (mode == "OFB"):
+        mode = AES.MODE_OFB
+    elif (mode == "CBC"):
+        mode = AES.MODE_CBC
+    elif (mode == "CTR"):
+        mode = AES.MODE_CTR
 
-	#Tao key 32bit bang ham bam tu key mac dinh
-	myHash = SHA256.new(myKey)
-	key = myHash.digest()
-	#Bam lai IV thanh 16 bytes
-	myHash = MD5.new(IV)
-	IV = myHash.digest()
-	
-	if (mode != AES.MODE_CTR):
-		encryptor = AES.new(key, mode, IV)
-	else:
-		ctr = Crypto.Util.Counter.new(128,
-                       initial_value=long(IV.encode("hex"), 16))
-		encryptor = AES.new(key, AES.MODE_CTR, counter=ctr)
+    myHash = SHA256.new(myKey)
+    key = myHash.digest()
+    # Make sure IV to be 16 bytes
+    myHash = MD5.new(IV)
+    IV = myHash.digest()
 
-	filesize = os.path.getsize(input_file)
-	#Doc va ghi file
-	with open(input_file, "rb") as fin:
-		with open(output_file,"wb") as fout:
-			fout.write(struct.pack('<Q',filesize))
-			fout.write(IV) 
+    if (mode != AES.MODE_CTR):
+        encryptor = AES.new(key, mode, IV)
+    else:
+        ctr = Crypto.Util.Counter.new(128,
+                                      initial_value=long(IV.encode("hex"), 16))
+        encryptor = AES.new(key, AES.MODE_CTR, counter=ctr)
 
-			#while - do: doc va ghi file theo block size
-			while True:
-				block = fin.read(blockSize)
-				#print "Block:", block
-				if len(block) == 0:
-					break
-				elif len(block) % 16 != 0:
-					#Them padding
-					block = block + padding * (16 -len(block) % 16)
-					#print "Block with pads:", block
-				fout.write(encryptor.encrypt(block))
-	return
+    filesize = os.path.getsize(input_file)
+    # Read and write input_file
+    with open(input_file, "rb") as fin:
+        with open(output_file, "wb") as fout:
+            fout.write(struct.pack('<Q', filesize))
+            fout.write(IV)
 
-#=======================================================
+            # while - do: read and write by block size
+            while True:
+                block = fin.read(blockSize)
+                if len(block) == 0:
+                    break
+                elif len(block) % 16 != 0:
+                    # Add padding
+                    block = block + padding * (16 - len(block) % 16)
+                fout.write(encryptor.encrypt(block))
+    return
+
+# =======================================================
+
+
 def main(argv):
-	#Xu ly tham so dong lenh
-	try:
-		#getopt.getopt(args, options, [long_options])
-		opts, args = getopt.getopt(argv,"m:k:i:")
-	except getopt.GetoptError:
-		print "encrypt.py –m <mode> –k <key> -i <IV> <input_file> <output_file>"
-		sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "m:k:i:")
+    except getopt.GetoptError:
+        print "encrypt.py –m <mode> –k <key> -i <IV> <input_file> <output_file>"
+        sys.exit(2)
 
-	#Neu danh sach tham so sau khi tru di cac option
-	#khong phai la dang: "encrypt.py <input_file> <output_file>"
-	if(len(args) != 2):
-		print "encrypt.py –m <mode> –k <key> -i <IV> <input_file> <output_file>"
-		sys.exit(2)
+    if(len(args) != 2):
+        print "encrypt.py –m <mode> –k <key> -i <IV> <input_file> <output_file>"
+        sys.exit(2)
 
-	#Lay thong tin input_file va output_file
-	input_file = args[0]
-	output_file = args[1]
+    # get input_file and output_file
+    input_file = args[0]
+    output_file = args[1]
 
-	#Chay vong for de lay option
-	for opt, arg in opts:
-		if opt == "-h":
-		  print "encrypt.py -m <mode> -k <key> -i <IV> <input_file> <output_file>"
-         	  sys.exit()
-		elif opt == "-m":
-			mode = arg.upper()
-		elif opt == "-k":
-			myKey = arg
-		elif opt == "-i":
-			IV = arg
-	#---------------Xu ly xong tham so dong lenh-------------------
+    # Get option
+    for opt, arg in opts:
+        if opt == "-h":
+            print "encrypt.py -m <mode> -k <key> -i <IV> <input_file> <output_file>"
+            sys.exit()
+        elif opt == "-m":
+            mode = arg.upper()
+        elif opt == "-k":
+            myKey = arg
+        elif opt == "-i":
+            IV = arg
 
-	#Goi ham ENCRYPT
-	encrypt(mode, myKey, IV, input_file, output_file)
+    # Run encrypt function
+    encrypt(mode, myKey, IV, input_file, output_file)
 
-	return
-#=================================================================
+    return
+# =================================================================
+
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
